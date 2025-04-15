61fdc71580c736796b99c68218cbb67d8040eec2
Dusty <dustin.forsyth@outlook.com>
2025-04-15 16:33:31 +0800

    updated

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,19 +1,38 @@
+
+import  {useEffect} from "react";
+import { useState } from "react";
+
 import { Card } from "../components/Card"
 export const MovieList = () => {
+  
+  
+  
+  const [movies, setMovies] = useState([]);
+
+      useEffect(() => {
+      async function fetchMovies() {
+      const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=2e0dbb7fbf1549ffa074dcb56280334b");
+      const data = await response.json();
+      setMovies(data.results);
+    }
+    fetchMovies();
+  }, [])
+  
+  
+  
   return (
     <main>
       
       <section className="max-w-7xl mx-auto py-7"> 
         <div className="flex justify-start flex-wrap ">
           
-              
 
-          <Card/>         
-          <Card/>  
-          <Card/>  
-          <Card/>  
-          <Card/>  
-          <Card/>   
+
+            { movies.map((movie) => (
+              <Card key={movie.id} movie={movie}/> 
+            ))}  
+
+                  
               
                                       
 
