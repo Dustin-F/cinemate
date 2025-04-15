6430162a675ffc48e6a48c6ba3c2b8ee417e4761
Dusty <99244375+Dustin-F@users.noreply.github.com>
2025-04-15 22:25:11 +0800

    footer update

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,45 +1,30 @@
-
-import  {useEffect} from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 
-import { Card } from "../components/Card"
+import { Card } from "../components/Card";
 export const MovieList = () => {
-  
-  
-  
   const [movies, setMovies] = useState([]);
 
-      useEffect(() => {
-      async function fetchMovies() {
-      const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=2e0dbb7fbf1549ffa074dcb56280334b");
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=2e0dbb7fbf1549ffa074dcb56280334b"
+      );
       const data = await response.json();
       setMovies(data.results);
     }
     fetchMovies();
-  }, [])
-  
-  
-  
+  }, []);
+
   return (
     <main>
-      
-      <section className="max-w-7xl mx-auto py-7"> 
+      <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-start flex-wrap ">
-          
-
-
-            { movies.map((movie) => (
-              <Card key={movie.id} movie={movie}/> 
-            ))}  
-
-                  
-              
-                                      
-
-
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
         </div>
       </section>
-      
-      </main>
-  )
-}
+    </main>
+  );
+};
