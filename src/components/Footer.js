13082bd49f27c6c486c595c789476b4c26a59a93
Dusty <dustin.forsyth@outlook.com>
2025-04-22 15:09:49 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,40 +15,7 @@ export const Footer = () => {
             </Link>
             . All Rights Reserved.
           </span>
-          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-            <li>
-              <a
-                href="#"
-                target="_blank"
-                className="hover:underline me-4 md:me-6"
-              >
-                Instagram
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                target="_blank"
-                className="hover:underline me-4 md:me-6"
-              >
-                LinkedIn
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                target="_blank"
-                className="hover:underline me-4 md:me-6"
-              >
-                Youtube
-              </a>
-            </li>
-            <li>
-              <a href="#" target="_blank" className="hover:underline">
-                Github
-              </a>
-            </li>
-          </ul>
+         
         </div>
       </footer>
     </footer>
